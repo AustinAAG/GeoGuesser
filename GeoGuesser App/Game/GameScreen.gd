ef8ee7c90f671158e9961bdf6810b88e0ab317d5
Austin <austin98@vt.edu>
2022-06-27 21:05:54 -0700

    now properly finds a usable location before requesting

--- GeoGuesser App/Game/GameScreen.gd
+++ GeoGuesser App/Game/GameScreen.gd
@@ -1,5 +1,6 @@
 extends Panel
 
+# Load in child nodes as variabels
 onready var streetView = $MarginContainer/StreetView
 onready var button1 = $Options/Panel/HBoxContainer/Button
 onready var button2 = $Options/Panel/HBoxContainer/Button2
@@ -8,29 +9,52 @@ onready var button4 = $Options/Panel/HBoxContainer/Button4
 onready var timer = $Countdown/Timer
 onready var streetViewContainer = $MarginContainer
 
-var data : PoolStringArray
-var original_url = "https://maps.googleapis.com/maps/api/streetview?size=600x300&location=40.170141,-76.994244&heading=151.78&pitch=-0.76&key="
-var img_url_passed
-var http_request
-var new_image_url
+#image variables
 var width
 var height
 var image
+
+# HTTP Request parsing
+var http_request1
+var http_request2
+var data : PoolStringArray
+
+# Street View Variables
+var image_url = "https://maps.googleapis.com/maps/api/streetview?size="
+var new_url
+var latitude
+var longitude
+export var st_view_width = "600"
+export var st_view_height = "300"
+export var heading = "0"
+export var fov = "90"
+export var pitch = "0"
+export var radius = "1000"
+
 var rng = RandomNumberGenerator.new()
 
+# Street View Metadata Variables
+var metadata_url = "https://maps.googleapis.com/maps/api/streetview/metadata?"
+var location_url
+var location_found = false
+
+# Constants
+var API_KEY = ""
+
 func _ready():
+	randomize()
 	# Create an HTTP request node and connect its completion signal.
-	rng.randomize()
-	http_request = HTTPRequest.new()
-	add_child(http_request)
-	http_request.connect("request_completed", self, "_http_request_completed")
-	var new_url = "https://maps.googleapis.com/maps/api/streetview?size=600x300&location=" + str(rng.randf_range(0.000000, 1.0)*180 - 90) + "," + str(rng.randf_range(0.000000, 1.0)*360 - 180) + "&heading=151.78&pitch=-0.76&key=INSERT_API_KEY_HERE&return_error_code=true"
-	print(new_url)
-	http_request(original_url)
+	http_request1 = HTTPRequest.new()
+	add_child(http_request1)
+	http_request1.connect("request_completed", self, "_http_request_completed1")
+	
+	http_request2 = HTTPRequest.new()
+	add_child(http_request2)
+	http_request2.connect("request_completed", self, "_http_request_completed2")
+	
+	new_location()
 
 func _physics_process(delta):
-#	print(streetViewContainer.get_size().x)
-#	print(streetViewContainer.get_size().y)
 	if width != streetViewContainer.get_size().x || height != streetViewContainer.get_size().y:
 		width = streetViewContainer.get_size().x
 		height = streetViewContainer.get_size().y
@@ -41,14 +65,14 @@ func _physics_process(delta):
 			streetView.texture = texture
 
 
-func http_request(url):
+func http_request1(url):
 	# Perform the HTTP request. The URL below returns a PNG and JPG image as of writing.
-	var error = http_request.request(url)
+	var error = http_request1.request(url)
 	if error != OK:
-		push_error("an error occured in the HTTP request.") 
+		push_error("an error occured in the HTTP request.")
 
 
-func _http_request_completed(result, response_code, headers, body):
+func _http_request_completed1(result, response_code, headers, body):
 	image = Image.new()
 	var error = image.load_jpg_from_buffer(body)
 	if error != OK:
@@ -61,6 +85,31 @@ func _http_request_completed(result, response_code, headers, body):
 	#Display the image in a TextureRect node.
 	streetView.texture = texture
 	timer.start()
+
+func http_request2(url):
+	# Perform the HTTP request. The URL below returns a PNG and JPG image as of writing.
+	var error = http_request2.request(url)
+	if error != OK:
+		push_error("an error occured in the HTTP request.")
+
+func _http_request_completed2(result, response_code, headers, body):
+	var response = body.get_string_from_utf8()
+	print("looking for location")
+	if "OK" in response:
+		print("location found")
+		st_view_width = str(streetViewContainer.get_size().x)
+		st_view_height = str(streetViewContainer.get_size().y)
+		new_url = image_url + st_view_width + "x" + st_view_height + "&location=" + latitude + "," + longitude + "&heading=" + heading + "&pitch=" + pitch + "&fov=" + fov + "&radius=" + radius + "&key=" + API_KEY
+		http_request1(new_url)
+	else:
+		print("looking for new location")
+		new_location()
+
+func new_location():
+	latitude = str(rng.randf_range(0.000000, 1.0)*180 - 90)
+	longitude = str(rng.randf_range(0.000000, 1.0)*360 - 180)
+	location_url = metadata_url + "&location=" + latitude + "," + longitude + "&heading=" + heading + "&pitch=" + pitch + "&fov=" + fov + "&radius=" + radius + "&key=" + API_KEY
+	http_request2(location_url)
 
 
 func _on_Button4_pressed():
@@ -80,5 +129,5 @@ func _on_Button_pressed():
 
 
 func _on_Countdown_timer_ended():
-	http_request(original_url)
-
+	new_location()
+	http_request1(new_url)
